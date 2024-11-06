6239e5ddb57b633f146906bc42fae943b638cac3
Manidweep <manidweep678@gmail.com>
2024-11-06 18:58:00 -0500

    kebab: Bringup Rising

--- lineage_kebab.mk
+++ lineage_kebab.mk
@@ -29,3 +29,19 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=OnePlus8T \
     SystemDevice=OnePlus8T \
     SystemName=OnePlus8T
+
+# RisingOS Flags
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+TARGET_PREBUILT_GOOGLE_CAMERA := false
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
+
+# Inherit RisingOS configurations.
+RISING_MAINTAINER = Lord Itachi
+
+# Add Official Stuff
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Qualcomm Snapdragon 865" \
+    RisingMaintainer="Lord Itachi"
